fa65a0701b0967756797f2432e590065db578a83
omaranBazna <omran4d@gmail.com>
2022-05-25 10:15:11 -0400

    Add Set Auth

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/Home"
 import Login from './pages/Login';
+import CreatePost from './pages/CreatePost';
 import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
 function App() {
   return (
     <div >
+      
      <Home  />
+     <Login />
+     <CreatePost />
 
     </div>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,24 @@
+import { signInWithPopup } from "firebase/auth";
 import React from "react"
+import {auth,provider} from "../firebase-config"
+import {signInWithPopup} from "firebase/auth"
 
-function Login(){
-    return <div> Login</div>
+
+function Login(setIsAuth){
+
+    const singInWithGoogel=()=>{
+        signInWithPopup(auth,provider).then((result)=>{
+   setIsAuth(true);
+
+        }
+        )
+    }
+    return (<div className="login-page"> 
+    <p> Sing In with google</p>
+    <button className="log-in-with-google" onClick={singInWithGoogel()}>
+        Sign In with Google
+    </button>
+    </div>);
 }
 
 export default Login;
